94f3be00ab50d0fd75ed407c088e0db1c8e47cf7
alva <alvarayhan333@gmail.com>
2024-03-04 16:40:13 +0700

    first commit

--- hhh.java
+++ hhh.java
@@ -4,6 +4,6 @@ public class hhh {
     public static void main(String[] args) {
         int[] angka = {1,2,3,4};
         System.out.println(angka.length);
-        
+        System.out.println("lupyu");
     }
 }
