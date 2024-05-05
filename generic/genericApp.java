3f80a4122057aafa9b04ca0aa77efdc1d8456e06
Vlaaming0 <125271532+Vlaaming0@users.noreply.github.com>
2024-05-06 02:13:44 +0700

    Delete generic directory

--- generic/genericApp.java
+++
@@ -1,8 +0,0 @@
-package generic;
-
-public class genericApp {
-    public static void main(String[] args) {
-        genericClass <Integer> Mydata = new genericClass<Integer>(69);
-        System.out.println(Mydata.getData());
-    }
-}
