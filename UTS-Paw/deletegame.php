3e0746755fa4c8edb883d9e9a323b21b17284091
Vlaaming0 <125271532+Vlaaming0@users.noreply.github.com>
2024-10-24 14:32:45 +0700

    Update deletegame.php

--- UTS-Paw/deletegame.php
+++ UTS-Paw/deletegame.php
@@ -7,20 +7,16 @@ if (!isset($_SESSION['username'])) {
 
 include 'config.php';
 
-// Cek apakah ada ID game yang dikirim
 if (isset($_GET['game_id'])) {
-    $id = intval($_GET['game_id']); // Ambil game_id dari URL
+    $id = intval($_GET['game_id']); 
 
-    // Query untuk menghapus game berdasarkan ID
     $stmt = $conn->prepare("DELETE FROM games WHERE game_id = ?");
     $stmt->bind_param("i", $id);
 
     if ($stmt->execute()) {
-        // Redirect ke halaman daftar game setelah berhasil dihapus
         header("Location: daftargame.php");
         exit();
     } else {
-        // Tampilkan pesan kesalahan jika terjadi error
         echo "Error: " . $stmt->error;
     }
 } else {
